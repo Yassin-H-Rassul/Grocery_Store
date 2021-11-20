aa8a416acf5420230b22e178ff00f533b41dca65
Hana Mohammad Abdullah <73670213+hanohirani@users.noreply.github.com>
2021-11-20 18:57:24 +0300

    adding the server
    this server class only cointain the database

---
+++ ServerNetworking/src/Model/Category.java
@@ -0,0 +1,66 @@
+package Model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+    int categoryID;
+    String categoryType;
+    String categoryDescription;
+
+
+
+    public Category(int categoryID, String categoryType, String categoryDescription){
+        super();
+        this.categoryID=categoryID;
+        this.categoryType=categoryType;
+        this.categoryDescription=categoryDescription;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
+
+
+    public String getCategoryType() {
+        return categoryType;
+    }
+
+    public void setCategoryType(String categoryType) {
+        this.categoryType = categoryType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return categoryID == category.categoryID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(categoryID);
+    }
+
+    @Override
+    public String toString() {
+        return
+                "\n              Category Infromation          \n------------------------------------------"+"\nScategoryID : " + categoryID +
+                        "\ncategoryType : " + categoryType +
+                        "\ncategoryDescription : " + categoryDescription +"\n------------------------------------------";
+    }
+}
