a3356514a93645df188295b04e6c7efa48865ee8
Hana Mohammad Abdullah <73670213+hanohirani@users.noreply.github.com>
2021-11-20 21:08:15 +0300

    Update
    changing some part of  view and  model

---
+++ Grocery/src/Main.java
@@ -0,0 +1,21 @@
+import Controller.viewController;
+import Model.Category;
+import Model.employee;
+//import View.ManagerEmploDemo;
+//import View.MnagerCategoryDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        //View Controller to change between the different screen
+        // viewController.ViewController();
+
+    }
+}
